c16ec3747dbb0d642ee7ac166109a3b382e34e75
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2024-06-23 22:56:30 +0100

    Feat: latest env

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigService, ConfigModule as NestConfigModule } from '@nestjs/config';
+import {
+  ConfigService,
+  ConfigModule as NestConfigModule,
+} from '@nestjs/config';
 import * as Joi from 'joi';
 
 @Module({
@@ -7,6 +10,7 @@ import * as Joi from 'joi';
     NestConfigModule.forRoot({
       isGlobal: true,
       cache: true,
+      //envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
       validationSchema: Joi.object({
         DATABASE_URL: Joi.string().required(),
         PORT: Joi.string().required(),
@@ -17,25 +21,32 @@ import * as Joi from 'joi';
           .valid('development', 'production')
           .default('development')
           .required(),
-        SWAGGER_DOC_TITLE: Joi.string().default('API Documentation'),
-        SWAGGER_DOC_DESCRIPTION: Joi.string().default('API Description'),
-        SWAGGER_DOC_VERSION: Joi.string().default('1.0'),
-        SWAGGER_PATH: Joi.string().default('documentation'),
-        SWAGGER_SITE_TITLE: Joi.string().default('API Docs'),
-        SWAGGER_MODELS_EXPAND_DEPTH: Joi.number().default(-1),
+          SWAGGER_DOC_TITLE: Joi.string().required(),
+        SWAGGER_DOC_DESCRIPTION: Joi.string().required(),
+        SWAGGER_DOC_VERSION: Joi.string().required(),
+        SWAGGER_PATH: Joi.string().required(),
+        SWAGGER_SITE_TITLE: Joi.string().required(),
+        SWAGGER_MODELS_EXPAND_DEPTH: Joi.number().required(),
       }),
       load: [
         () => ({
           swagger: {
             docTitle: process.env.SWAGGER_DOC_TITLE,
-            docDescription: process.env.SWAGGER_DOC_DESCRIPTION,
+            docDescription:
+              process.env.SWAGGER_DOC_DESCRIPTION,
             docVersion: process.env.SWAGGER_DOC_VERSION,
             path: process.env.SWAGGER_PATH,
             siteTitle: process.env.SWAGGER_SITE_TITLE,
-            defaultModelsExpandDepth: parseInt(process.env.SWAGGER_MODELS_EXPAND_DEPTH, 10),
+            defaultModelsExpandDepth: process.env.SWAGGER_MODELS_EXPAND_DEPTH
+              ? parseInt(process.env.SWAGGER_MODELS_EXPAND_DEPTH, 10)
+              : -1,
           },
         }),
       ],
+      // validationOptions: {
+      //   allowUnknown: false,
+      //   abortEarly: true,
+      // },
     }),
   ],
   providers: [ConfigService],
