09d51d896c14febd1e7b5fd3fdf52776d7c553e2
IgnatiusFrancis <ignatiuzzfrank@gmail.com>
2024-07-24 10:34:16 +0100

    Feat: ContactUs setup

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { setupSwagger } from './config';
-// import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,7 +15,6 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
-
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -39,6 +37,5 @@ async function bootstrap() {
   await app.listen(configService.get('PORT'), () => {
     return logger.log(`🚀 Server running on port ${configService.get('PORT')}`);
   });
-
 }
 bootstrap();
